49976d2e4e3a3df18f8da7ac7165863495dfea02
torrient <hoangthuyen1998@gmail.com>
2022-11-05 19:04:52 +0700

    release

--- include/yolov5face_onnx.h
+++ include/yolov5face_onnx.h
@@ -22,7 +22,6 @@ class YOLO
 public:
 	YOLO(Net_config config);
 	void detect(std::vector<cv::Mat>& images);
-	cv::Mat faceAlign(cv::Mat src_image, cv::Mat& landmark_from);
 private:
 	const float anchors[3][6] = { {4,5,  8,10,  13,16}, {23,29,  43,55,  73,105},{146,217,  231,300,  335,433} };
 	const float stride[3] = { 8.0, 16.0, 32.0 };
